75b645d87e8f2fe060063f00b9f1d3e56dd381e2
oleksandr-ishchuk <alexmway@mail.ru>
2023-07-21 23:51:56 +0200

    add property of navbar in abstract class one

--- modules/class/nonBD/navBootstrap/INavMenu.php
+++ modules/class/nonBD/navBootstrap/INavMenu.php
@@ -16,8 +16,44 @@ namespace class\nonBD\navBootstrap;
 abstract class INavMenu
 {
     private $nameButton;
-    public function getNameButton()
-    {
-        return $this->nameButton;
-    }
+
+    /**
+     * Данные свойства - это в основном классы стилизации навмгационного меню от бутстрапа
+     * Свойствам заданы значения по умолчанию, которые доступны для потомков
+     * Если будут изменения данных свойств, то они будут делаться через
+     * контейнер свойств Синглтон.
+     */
+    protected $navbar='navbar';
+    protected $navbarExpandLg='navbar-expand-lg';
+    protected $navbarLight='navbar-light';
+    protected $bgLight='bg-light';
+    protected $containerFluid='container-fluid';
+    protected $navbarBrand='navbar-brand';
+    protected $navbarBrandHref='#';
+    protected $navbarBrandName='Navbar';
+    protected $navbarToggler='navbar-toggler';
+    protected $navbarTogglerIcon='navbar-toggler-icon';
+    protected $navbarTogglerIconSpan='';
+    protected $collapse='collapse';
+    protected $navbarCollapse='navbar-collapse';
+    protected $navbarNav='navbar-nav';
+    protected $meAuto='me-auto';
+    protected $mb2='mb-2';
+    protected $mbLg0='mb-lg-0';
+    protected $navItem='nav-item';
+    protected $navLink='nav-link';
+    protected $active='active';
+    protected $dropdown='dropdown';
+    protected $dropdownToggle='dropdown-toggle';
+    protected $dropdownMenu='dropdown-menu';
+    protected $dropdownDivider='dropdown-divider';
+    protected $dFlex='d-flex';
+    protected $formControl='form-control';
+    protected $me2='me-2';
+    protected $btn='btn';
+    protected $btnOutlineSuccess='btn-outline-success';
+
+
+    abstract public function getNameButton();
+
 }
