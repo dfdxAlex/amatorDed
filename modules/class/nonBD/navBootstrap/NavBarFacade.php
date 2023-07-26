e5a5fe8a9b34052305f9681d90a5e80710e668e5
oleksandr-ishchuk <alexmway@mail.ru>
2023-07-26 22:58:33 +0200

    add menu psttern and navbar

--- modules/class/nonBD/navBootstrap/NavBarFacade.php
+++ modules/class/nonBD/navBootstrap/NavBarFacade.php
@@ -1,6 +1,6 @@
 <?php
 namespace class\nonBD\navBootstrap;
-// class\nonBD\navBootstrap\NavBarFacade::createNavBar();
+
 class NavBarFacade
 {
     public static function createNavBar()
@@ -13,12 +13,8 @@ class NavBarFacade
         $obj->setProperty('button-search',false);
 
         /**
-         * Создается простая кнопка
-         */
-
-
-        /**
          * Создается простая кнопка для выпадающего меню
+         * Выпадающее меню Library PHP
          */
         $obj->setProperty('Home','Library PHP');
         $obj->setProperty('work-box',true);
@@ -29,16 +25,37 @@ class NavBarFacade
         $obj->setProperty('work-box',true);
         $button2 = new ElementNavBar($obj);
 
-
-        /**
-         * создать объект контейнер для выпадающего меню
-         */
         $oblBox = new BoxNavMenu($obj);
         $oblBox->addElement($button1);
         $oblBox->addElement($button2);
 
-        $obj->addElement($oblBox);
+        /**
+         * Создание меню patterns
+         */
+        $obj->setProperty('Home','Patterns');
+        $obj->setProperty('work-box',true);
+        $patterns1 = new ElementNavBar($obj);
 
+        $obj->setProperty('Home','Simple Factory');
+        $obj->setProperty('link','?pattern1');
+        $obj->setProperty('work-box',true);
+        $patterns2 = new ElementNavBar($obj);
+
+        $pattersObj = new BoxNavMenu($obj);
+        $pattersObj->addElement($patterns1);
+        $pattersObj->addElement($patterns2);
+
+
+
+        /**
+         * Поместить объекты в главный объект
+         */
+        $obj->addElement($oblBox);
+        $obj->addElement($pattersObj);
+
+        /**
+         * Вывести меню на страницу
+         */
         $obj->writeElement($obj);
     }
 }
