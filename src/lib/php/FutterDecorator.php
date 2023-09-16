08c74b369ef4e453a2fd9f933050c14d16510e43
oleksandr-ishchuk <alexmway@mail.ru>
2023-09-16 22:43:51 +0200

    refactoring class FutterDecorator

--- src/lib/php/FutterDecorator.php
+++ src/lib/php/FutterDecorator.php
@@ -13,109 +13,151 @@ class FutterDecorator extends Futter
 {
     public function __construct()
     {
+     $arrayParametr = $this->returnArrayWithParametrForFooter();
 
-    // new ConnectFunctionJsLoad('searchSection');
+     echo $this->returnFooterHtml($arrayParametr);
+
+     $this->connectJsClasses();
+
+     $this->printConnectingClasses(false);
+
+     parent::__construct();
+    }
+
     /**
-     * Класс для работы с переводом текста на разные языки
-     * Класс из общей библиотеки, но немного переделанный
-     * Класс DelegatorLang регистрируется в контейнере
-     * Для использования из контейнера запустить метод ->translator()
-     * Чтобы добавить перевод ->control(true);
-     * Чтобы посмотреть или удалить перевод ->echoDataMas(); запустить
-     * \src\lib\php\ContainerObject::getInstance()->getProperty('DelegatorLang');
+     * 
      */
-     $nomerToClass = ContainerObject::getInstance()
-                                  ->getProperty('TranslateFacade')
-                                  ->translator('Число подключенных классов');
-     $goToYoutube = ContainerObject::getInstance()
-                                  ->getProperty('TranslateFacade')
-                                  ->translator('Перейти на Youtube канал');
-     $goToGitHub = ContainerObject::getInstance()
-                                  ->getProperty('TranslateFacade')
-                                  ->translator('Перейти на GitHub');
-     $technologies = ContainerObject::getInstance()
-                                  ->getProperty('TranslateFacade')
-                                  ->translator('Использованные технологии');
-     $pattern = ContainerObject::getInstance()
-                                  ->getProperty('TranslateFacade')
-                                  ->translator('Применённые шаблоны');
-           echo '
-              <footer id="footer">
-              <div class="row">
-                <div class="col-sm-12 col-md-6">
-                  <a 
-                    href="https://www.youtube.com/channel/UCsIw_8Tx-R3ZKEcwvw5oGzA"
-                    target="_blank"
-                  >
-                    '.$goToYoutube.'
-                  </a>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                  <a 
-                    href="https://github.com/dfdxAlex"
-                    target="_blank"
-                  >
-                    '.$goToGitHub.'
-                  </a>
-                </div>
-              </div>
-              <br>
-              <div class="row">
-                <div class="col-sm-12 col-md-4">
-                  '.$technologies.':<br>
-                  -MySql<br>
-                  -PHP<br>
-                  -HTML5<br>
-                  -CSS3<br>
-                  -JavaScript<br>
-                  -jQuery<br>
-                </div>
-                <div class="col-sm-12 col-md-4">
-                  '.$pattern.':<br>
-                  -Pattern Composite<br>
-                  -Pattern Decorator<br>
-                  -Pattern Dependency Injection<br>
-                  -Property Container<br>
-                  -Singleton<br>
-                  -Facade<br>
-                  -Service Locator<br>
-                  -Simple Factory<br>
-                </div>
-                <div class="col-sm-12 col-md-4">
-                  Frameworks:<br>
-                  -BootStrap 5<br>
-                </div>
-              </div>
-              <div class="row">
-                 <div class="col-12">
-                   '.$nomerToClass.': '.
-                   Statistic::getLinkStatistic()->getPlusOneToIntObj()
-                   .'
-                 </div>
-              </div>
-              </footer>
-        ';
+    private function connectJsClasses()
+    {
+      new ImportFunctionsJs('load',
+                            'searchSection',
+                            'bagList'
+                           );
 
-       new ImportFunctionsJs('load',
-                             'searchSection',
-                             'bagList'
-                            );
-
-       new ImportFunctionsJs('non',
-                             'bag\CoderDeCoder',
-                             'bag\Cookies',
-                             'bag\Bag',
-                             'bag\VievBag'
-                            );
-       
-
+      new ImportFunctionsJs('non',
+                            'bag\CoderDeCoder',
+                            'bag\Cookies',
+                            'bag\Bag',
+                            'bag\VievBag'
+                           );
+    }
+    /**
+     * 
+     */
+    private function printConnectingClasses($noPrint)
+    {
+        if (!$noPrint) return;
         /**
         * Смотрим проперти контейнер загруженных классов
         */
-       // $rez = src\lib\php\Statistic::getLinkStatistic()->getListClass();
-       // foreach($rez as $key=>$val) {
-       //     echo "$key=>$val<br>";
-       // }
-       parent::__construct();
+        $rez = Statistic::getLinkStatistic()->getListClass();
+        echo '<div class="print-connecting-classes">';
+        foreach($rez as $key=>$val) {
+            echo "$key=>$val<br>";
+        }
+        echo '</div>';
+    }
+    /**
+     * 
+     */
+    private function returnArrayWithParametrForFooter()
+    {
+      $nomerToClass = ContainerObject::getInstance()
+                      ->getProperty('TranslateFacade')
+                      ->translator('Число подключенных классов');
+      $goToYoutube = ContainerObject::getInstance()
+                      ->getProperty('TranslateFacade')
+                      ->translator('Перейти на Youtube канал');
+      $goToGitHub = ContainerObject::getInstance()
+                      ->getProperty('TranslateFacade')
+                      ->translator('Перейти на GitHub');
+      $technologies = ContainerObject::getInstance()
+                      ->getProperty('TranslateFacade')
+                      ->translator('Использованные технологии');
+      $pattern = ContainerObject::getInstance()
+                      ->getProperty('TranslateFacade')
+                      ->translator('Применённые шаблоны');
+      $linkStatistic = Statistic::getLinkStatistic()
+                      ->getPlusOneToIntObj();
+
+      $arrayParametr = [
+      $nomerToClass,
+      $goToYoutube,
+      $goToGitHub,
+      $technologies,
+      $pattern,
+      $linkStatistic
+      ];
+
+      return $arrayParametr;
+    }
+    /**
+     * 
+     */
+    private function returnFooterHtml($arrayParametr) 
+    {
+       [$nomerToClass,
+        $goToYoutube,
+        $goToGitHub,
+        $technologies,
+        $pattern,
+        $linkStatistic] = $arrayParametr;
+
+      return '
+      <footer id="footer">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <a 
+            href="https://www.youtube.com/channel/UCsIw_8Tx-R3ZKEcwvw5oGzA"
+            target="_blank"
+          >
+            '.$goToYoutube.'
+          </a>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <a 
+            href="https://github.com/dfdxAlex"
+            target="_blank"
+          >
+            '.$goToGitHub.'
+          </a>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          '.$technologies.':<br>
+          -MySql<br>
+          -PHP<br>
+          -HTML5<br>
+          -CSS3<br>
+          -JavaScript<br>
+          -jQuery<br>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          '.$pattern.':<br>
+          -Pattern Composite<br>
+          -Pattern Decorator<br>
+          -Pattern Dependency Injection<br>
+          -Property Container<br>
+          -Singleton<br>
+          -Facade<br>
+          -Service Locator<br>
+          -Simple Factory<br>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          Frameworks:<br>
+          -BootStrap 5<br>
+        </div>
+      </div>
+      <div class="row">
+         <div class="col-12">
+           '.$nomerToClass.': '.
+           $linkStatistic
+           .'
+         </div>
+      </div>
+      </footer>
+     ';
     }
 }
